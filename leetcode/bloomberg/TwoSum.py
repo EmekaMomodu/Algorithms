efedd1d400d0f35b31027bba69d7e9f771f6b215
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2021-12-22 10:26:14 +0100

    two sum algorithm leetcode bloomberg

---
+++ leetcode/bloomberg/TwoSum.py
@@ -0,0 +1,59 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+"""
+
+
+class Solution(object):
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+    # where n is the length of nums
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        numsDict = {}
+        for firstIndex in range(len(nums)):
+            firstNum = nums[firstIndex]
+            secondNum = target - firstNum
+            secondIndex = numsDict.get(secondNum)
+            if secondIndex is not None:
+                return [firstIndex, secondIndex]
+            numsDict[firstNum] = firstIndex
+        return []
+
+
+
+
+

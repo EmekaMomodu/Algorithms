f8b9a5a720a03b3f949263653ebec0f81889614c
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2022-03-27 16:25:19 +0100

    some more algo

---
+++ leetcode/bloomberg/ValidParentheses.py
@@ -0,0 +1,45 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+
+"""
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parenDict = {'(': ')', '{': '}', '[': ']'}
+        stack = []
+        for paren in s:
+            if paren in parenDict:
+                stack.append(paren)
+            elif stack and parenDict[stack[-1]] == paren:
+                stack.pop()
+            else:
+                return False
+        if len(stack) != 0:
+            return False
+        return True
